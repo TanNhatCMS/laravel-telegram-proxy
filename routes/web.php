675a01ba8d69ab80217eeaf4a379b49c49e68547
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-05-28 01:41:31 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TelegramProxyController;
 
-Route::get('/', [TelegramProxyController::class, 'documentation']);
+Route::get('/', [TelegramProxyController::class, 'index'])->name('home');
 
 Route::any('/bot{token}/{method?}', [TelegramProxyController::class, 'proxy'])
     ->where('method', '.*');

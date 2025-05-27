1f2b10d9c50ac6591340e4865bb1ebb8e94afa05
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-05-28 02:11:45 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,63 @@
 <?php
 
+use App\Http\Middleware\CORS;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+use Illuminate\Foundation\Http\Middleware\TrimStrings;
+use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
+use Illuminate\Http\Middleware\SetCacheHeaders;
+use Illuminate\Http\Middleware\TrustProxies;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Routing\Middleware\ValidateSignature;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Middleware toàn cục (global middleware)
+        $middleware->append(CORS::class);
+        $middleware->append(TrustProxies::class);
+        $middleware->append(CheckForMaintenanceMode::class);
+        $middleware->append(ValidatePostSize::class);
+        $middleware->append(TrimStrings::class);
+        $middleware->append(ConvertEmptyStringsToNull::class);
+
+        $middleware->group('web', [
+        ]);
+
+
+        $middleware->alias([
+            'auth.basic' => AuthenticateWithBasicAuth::class,
+            'bindings' => SubstituteBindings::class,
+            'cache.headers' => SetCacheHeaders::class,
+            'can' => Authorize::class,
+            'signed' => ValidateSignature::class,
+            'throttle' => ThrottleRequests::class,
+            'verified' => EnsureEmailIsVerified::class,
+        ]);
+
+        $middleware->priority([
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            ThrottleRequests::class,
+            AuthenticateSession::class,
+            SubstituteBindings::class,
+            Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
